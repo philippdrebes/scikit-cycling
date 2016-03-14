7904510136699d700c2c9e5cc051b5f86f1b3763
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-03-14 21:09:09 +0100

    Forgot to put fitparse as relative path

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -78,4 +78,4 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 python setup.py develop
-python /third-party/python-fitparse/setup.py install
+python ./third-party/python-fitparse/setup.py install
