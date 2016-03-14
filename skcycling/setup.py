48693e11ac89dd2ae9cba03a56cbb7256961f5ea
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-03-15 00:36:42 +0100

    Add explecitely the tests directories

--- skcycling/setup.py
+++ skcycling/setup.py
@@ -7,26 +7,14 @@ def configuration(parent_package='', top_path=None):
     config = Configuration('skcycling', parent_package, top_path)
 
     config.add_subpackage('metrics')
+    config.add_subpackage('metrics/tests')
     config.add_subpackage('power_profile')
+    config.add_subpackage('power_profile/tests')
     config.add_subpackage('restoration')
+    config.add_subpackage('restoration/tests')
     config.add_subpackage('utils')
+    config.add_subpackage('utils/tests')
 
-    def add_test_directories(arg, dirname, fnames):
-        if dirname.split(os.path.sep)[-1] == 'tests':
-            config.add_data_dir(dirname)
-
-    # Add test directories
-    from os.path import isdir, dirname, join
-
-    def rel_isdir(curpath, d):
-        return isdir(join(curpath, d))
-
-    curpath = join(dirname(__file__), './')
-    subdirs = [join(d, 'tests') for d in os.listdir(curpath)
-               if rel_isdir(curpath, d)]
-    subdirs = [d for d in subdirs if rel_isdir(curpath, d)]
-    for test_dir in subdirs:
-        config.add_data_dir(test_dir)
     return config
 
 if __name__ == "__main__":
