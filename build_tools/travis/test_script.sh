212444ce85558775107c4eb103d3d86b1c6efa0f
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-04-06 02:20:07 +0200

    Try to fix travis because of the cache

--- build_tools/travis/test_script.sh
+++ build_tools/travis/test_script.sh
@@ -21,9 +21,9 @@ python -c "import scipy; print('scipy %s' % scipy.__version__)"
 python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
 
 if [[ "$COVERAGE" == "true" ]]; then
-   nosetests -s --with-coverage --with-timer --timer-top-n 20 skcycling
+   nosetests -v -s --with-coverage --cover-package=$MODULE $MODULE
 else
-   nosetests -s --with-timer --timer-top-n 20 skcycling
+   nosetests -v -s $MODULE
 fi
 
 # Is directory still empty ?
